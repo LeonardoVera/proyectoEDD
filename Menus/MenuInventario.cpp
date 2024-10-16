0912bba326709947026b12def405ca118cd285f5
Marco Renato <marco.castilla@unmsm.edu.pe>
2024-10-16 02:06:27 -0500

    Mejora en el menú (estetica)

--- Menus/MenuInventario.cpp
+++ Menus/MenuInventario.cpp
@@ -1,31 +1,31 @@
 #include "MenuInventario.h"
 
 MenuInventario::~MenuInventario() {
-    cout << "Saliendo del menú de inventario" << endl;
+    cout << "Saliendo del menu de inventario" << endl;
     system("pause");
 }
 void MenuInventario::mostrarMenu() {
-    gotoxy(32, 5);
+    gotoxy(32, 6);
     cout << "1. Agregar materia prima";
-    gotoxy(32, 7);
+    gotoxy(32, 8);
     cout << "2. Mostrar inventario de materia prima";
-    gotoxy(32, 9);
+    gotoxy(32, 10);
     cout << "3. Eliminar materia prima";
-    gotoxy(32, 11);
-    cout << "4. Regresar al menú principal";
-    gotoxy(32, 13);
-    cout << "Seleccione una opción (1-4): ";
+    gotoxy(32, 12);
+    cout << "4. Regresar al menu principal";
+    gotoxy(32, 14);
+    cout << "Seleccione una opcion (1-4): ";
 }
 
 void MenuInventario::iniciarMenu() {
     int opcion;
     do {
         system("cls");
-        gotoxy(28, 2);
+        gotoxy(43, 2);
         cout << "=============================";
-        gotoxy(30, 3);
-        cout << "MENÚ DE INVENTARIO";
-        gotoxy(28, 4);
+        gotoxy(50, 3);
+        cout << "MENU DE INVENTARIO";
+        gotoxy(43, 4);
         cout << "=============================";
         mostrarMenu();
         cin >> opcion;
